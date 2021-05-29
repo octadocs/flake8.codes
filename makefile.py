9e1c8fe259740b95e830229be83bd9208a4b32d5
Anatoly Scherbakov <altaisoft@gmail.com>
2021-05-29 21:07:47 +0700

    Manually create a CLI for Jeeves

--- makefile.py
+++ makefile.py
@@ -1,9 +1,20 @@
+import os
 import re
 import subprocess
-from dataclasses import dataclass, make_dataclass, field
-from typing import Any, Type
+import uuid
+from dataclasses import dataclass, make_dataclass, field, asdict, fields
+from typing import Any, Type, Dict, Optional
 
 from dependencies import Injector
+from typer import Typer
+
+
+def shallow_dict(data_cls) -> Dict[str, Any]:
+    """Equivalent to dataclasses.asdict but with no recursion."""
+    return {
+        dataclass_field.name: getattr(data_cls, dataclass_field.name)
+        for dataclass_field in fields(data_cls)
+    }
 
 
 @dataclass
@@ -12,27 +23,27 @@ class Shell:
 
     def __call__(self) -> None:
         """Execute the provided shell command in a subprocess."""
-        subprocess.run(('sh', '-c', self.command))
+        command = self.command.format(**shallow_dict(self))
+        subprocess.run(('sh', '-c', command))
 
 
-def shell(command: str) -> Type[Shell]:
-    match = re.search(
+def shell(command: str, description: Optional[str] = None) -> Type[Shell]:
+    arguments = re.findall(
         '{([^}]+)}',
         command,
     )
-
-    if match:
-        arguments = match.groups()
-    else:
-        arguments = ()
 
     argument_fields = [
         (argument, Any)
         for argument in arguments
     ]
 
+    uid = uuid.uuid4().hex[:5]
     return make_dataclass(
-        cls_name='Cls',
+        cls_name=f'_ShellCommand_{uid}',
+        namespace={
+            '__doc__': description,
+        },
         fields=[
             *argument_fields,
             ('command', 'str', field(default=command)),
@@ -41,20 +52,44 @@ def shell(command: str) -> Type[Shell]:
     )
 
 
+@dataclass(repr=False)
+class FlakehellCommand:
+    in_pycharm: bool
+
+    def __str__(self):
+        if self.in_pycharm:
+            return 'flake8helled'
+        else:
+            return 'flakehell lint'
+
+
+def is_pycharm() -> bool:
+    """Determine if the script is being called from PyCharm."""
+    return os.getenv('PYCHARM_HOSTED') is not None
+
+
 class Flake8Codes(Injector):
     """Jeeves helper for the project."""
 
     project_directory = 'flake8_codes'
+    in_pycharm = is_pycharm()
+
+    flakehell_command = FlakehellCommand
     lint = shell(
-        'git diff origin/master | flakehell lint --diff {project_directory}',
+        'git diff origin/master | {flakehell_command} --diff {project_directory}',
+        description='Verify correctness of Python code with flake8 & friends.'
     )
-    format = shell('isort {project_directory}')
+
+    format = shell(
+        'isort {project_directory}',
+        description='Isort imports in project.'
+    )
 
 
-def main():
-    # Flake8Codes.lint()
-    Flake8Codes.format()
+cli = Typer()
+cli.command(name='lint', short_help=Flake8Codes.lint.__doc__)(Flake8Codes.lint)
+cli.command(name='format', short_help=Flake8Codes.format.__doc__)(Flake8Codes.format)
 
 
 if __name__ == '__main__':
-    main()
+    cli()
