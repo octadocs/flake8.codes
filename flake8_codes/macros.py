a59b805eb8ca07597ee5db4847bcb9ae89f31268
Anatoly Scherbakov <altaisoft@gmail.com>
2021-05-29 15:45:35 +0700

    Make `{{ wps.config() }}` macro functional

--- flake8_codes/macros.py
+++ flake8_codes/macros.py
@@ -1,9 +1,30 @@
+from functools import partial
+
 from mkdocs_macros.plugin import MacrosPlugin
+from more_itertools import first
+from octadocs.octiron import Octiron
+from rdflib import URIRef
 
 
-def wps_config(url: str) -> str:
+def wps_config(
+    url: str,
+    octiron: Octiron,
+) -> str:
     """Render a link to a WPS configuration parameter page."""
-    return '**[DATA EXPUNGED]**'
+    config = first(octiron.query(
+        '''
+        SELECT * WHERE {
+            ?page
+                :about ?parameter ;
+                :name ?name ;
+                :value ?value ;
+                octa:url ?url .
+        }
+        ''',
+        parameter=URIRef(url + '/'),   # FIXME THIS!!!
+    ))
+
+    return f"[{config['name']}]({config['url']}) = `{config['value']}`"
 
 
 def define_env(env: MacrosPlugin) -> MacrosPlugin:
@@ -15,7 +36,10 @@ def define_env(env: MacrosPlugin) -> MacrosPlugin:
     more information.
     """
     env.variables['wps'] = {
-        'config': wps_config,
+        'config': partial(
+            wps_config,
+            octiron=env.variables.octiron,
+        ),
     }
 
     return env
