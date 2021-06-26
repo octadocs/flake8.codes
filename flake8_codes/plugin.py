0e4de97f9350f3c6b2bf785c451b163694c61662
Anatoly Scherbakov <altaisoft@gmail.com>
2021-06-26 15:18:56 +0700

    Rewrite page URLs in a more user-friendly way

--- flake8_codes/plugin.py
+++ flake8_codes/plugin.py
@@ -1,31 +1,53 @@
+import json
+import operator
 from pathlib import Path
+from typing import Iterable, cast, List
 
+import rdflib
 from mkdocs.plugins import BasePlugin
 from mkdocs.structure.files import File, Files
+from mkdocs.structure.nav import Navigation
+from octadocs.octiron import Octiron
 
 
 class Flake8Codes(BasePlugin):
-    def on_files(self, files: Files, config):
+    # def on_files(
+    #     self,
+    #     files: Files,
+    #     config,
+    # ):
+    def on_nav(
+        self,
+        nav: Navigation,
+        config,
+        files: Files,
+    ):
         """Publish the violation pages of every flake8 plugin in the root."""
-        wps_violations = [
-            mkdocs_file for mkdocs_file in files
-            if not mkdocs_file.src_path.endswith('index.md') and (
-                mkdocs_file.src_path.startswith(
-                    'wemake-python-styleguide/0.15.2/violations',
-                ) or mkdocs_file.src_path.startswith(
-                    'flake8-bugbear/20.11.1',
-                )
-            )
-        ]
+        octiron: Octiron = config['extra']['octiron']
+        site_dir = Path(config['site_dir'])
 
-        violation_file: File
-        for violation_file in wps_violations:
-            destination_path = Path(violation_file.dest_path)
+        pages_and_urls = octiron.query(
+            'SELECT ?page ?url WHERE { ?page octa:url ?url }',
+        )
 
-            violation_file.dest_path = str(destination_path.relative_to(
-                destination_path.parent.parent,
-            ))
-            violation_file.abs_dest_path = str(
-                Path(config['site_dir']) / violation_file.dest_path,
-            )
-            violation_file.url = f'{violation_file.name}/'
+        url_by_page = {
+            row['page'].toPython().replace(
+                'local:',
+                '',
+            ): row['url'].toPython().lstrip('/')
+            for row in pages_and_urls
+        }
+
+        mkdocs_file: File
+        for mkdocs_file in files:
+            url_from_graph = url_by_page.get(mkdocs_file.src_path)
+
+            if not url_from_graph:
+                continue
+
+            if url_from_graph == mkdocs_file.url:
+                continue
+
+            mkdocs_file.dest_path = url_from_graph + 'index.html'
+            mkdocs_file.abs_dest_path = str(site_dir / mkdocs_file.dest_path)
+            mkdocs_file.url = url_from_graph
